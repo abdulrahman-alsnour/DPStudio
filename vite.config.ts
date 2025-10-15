ed21ce08012dad031dfa909541479f72228ffa3d
abdulrahman-alsnour <abdnsour01@gmail.com>
2025-10-16 01:57:30 +0300

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/DPS/',
+  base: '/DPStudio/',
   server: {
     host: "::",
     port: 8080,
